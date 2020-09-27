58a4ee8642b1fa81fc1351e9283849d48b10c8f4
Kevin Del Castillo <quebin31@gmail.com>
2020-09-27 14:51:50 -0500

    Add capabilities enum.

--- src/capabilities.rs
+++ src/capabilities.rs
@@ -1,10 +1,16 @@
 use num_derive::{FromPrimitive, ToPrimitive};
 
+/// Possible device capabilities.
 #[derive(Debug, Clone, FromPrimitive, ToPrimitive)]
 pub enum DeviceCap {
+    /// This capability tells whether you can get partial objects.
     GetPartialObject = 0,
+    /// This capability tells whether you can send partial objects.
     SendPartialObject,
+    /// This capability tells whether you can edit objects in-place on a device.
     EditObjects,
+    /// This capability tells whether you can move an object.
     MoveObject,
+    /// This capability tells whether you can copy an object.
     CopyObject,
 }
