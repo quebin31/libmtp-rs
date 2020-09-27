5fd57d6cd90b7334d096134d48dd1325fe2913ba
Kevin Del Castillo <quebin31@gmail.com>
2020-09-27 14:52:06 -0500

    Added StackError and renamed Error to ErrorKind.

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,9 @@
-use std::string::FromUtf8Error;
-
 use libmtp_sys as ffi;
-use thiserror::Error as DError;
+use std::string::FromUtf8Error;
+use thiserror::Error;
 
-#[derive(Debug, Clone, DError)]
-pub enum Error {
+#[derive(Debug, Clone, Error)]
+pub enum ErrorKind {
     #[error("Unknown error!")]
     Unknown,
     #[error("")]
@@ -27,7 +26,7 @@ pub enum Error {
     UTF8Error { source: FromUtf8Error },
 }
 
-impl Error {
+impl ErrorKind {
     pub(crate) fn from_code(error_code: ffi::LIBMTP_error_number_t) -> Option<Self> {
         match error_code {
             ffi::LIBMTP_error_number_t_LIBMTP_ERROR_NONE => None,
@@ -48,8 +47,34 @@ impl Error {
     }
 }
 
-impl From<FromUtf8Error> for Error {
+impl From<FromUtf8Error> for ErrorKind {
     fn from(source: FromUtf8Error) -> Self {
         Self::UTF8Error { source }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct StackError {
+    pub kind: ErrorKind,
+    pub text: String,
+}
+
+impl StackError {
+    pub(crate) unsafe fn from_error_list(mut list: *const ffi::LIBMTP_error_t) -> Vec<Self> {
+        let mut stack = Vec::new();
+
+        while !list.is_null() {
+            let error_t = &*list;
+            let vec = c_charp_to_u8v!(error_t.error_text);
+
+            stack.push(StackError {
+                kind: ErrorKind::from_code(error_t.errornumber).expect("Unexpected code"),
+                text: String::from_utf8(vec).expect("Invalid UTF-8 on StackError"),
+            });
+
+            list = (*list).next;
+        }
+
+        stack
     }
 }
