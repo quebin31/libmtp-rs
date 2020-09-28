e1f5e1427a3ffa883f4425f2cdcf1ea4da61f577
Kevin Del Castillo <quebin31@gmail.com>
2020-09-27 23:31:18 -0500

    Check the error stack and grab the latest error. Also clean the whole error stack (seems like it's okay?). Use the latest error in other places.

--- src/basic/device.rs
+++ src/basic/device.rs
@@ -1,11 +1,9 @@
-use super::storage::Storage;
-use crate::{
-    capabilities::DeviceCap, error::ErrorKind, error::StackError, filetypes::Filetype,
-    properties::Property,
-};
+use super::{storage::Storage, storage::StoragePool, values::AllowedValues};
+use crate::{capabilities::DeviceCap, error::Error, filetypes::Filetype, properties::Property};
 use libmtp_sys as ffi;
 use num_derive::ToPrimitive;
 use num_traits::{FromPrimitive, ToPrimitive};
+use std::fmt::{self, Debug};
 
 /// Sorting types when updating the inner storage list of an MTP device.
 #[derive(Debug, Clone, Copy, ToPrimitive)]
@@ -22,8 +20,6 @@ pub enum UpdateStorage {
     Success,
     /// Partial success, couldn't get storage properties.
     OnlyIds,
-    /// Something were wrong.
-    Failure,
 }
 
 /// Check if a specific device, given its bus and device number, has an
@@ -46,11 +42,11 @@ pub enum BatteryLevel {
 
 /// Result from opening a raw device descriptor, holds information
 /// about the device, storage, etc.
-pub struct MTPDevice {
+pub struct MtpDevice {
     pub(crate) inner: *mut ffi::LIBMTP_mtpdevice_t,
 }
 
-impl Drop for MTPDevice {
+impl Drop for MtpDevice {
     fn drop(&mut self) {
         unsafe {
             ffi::LIBMTP_Release_Device(self.inner);
@@ -58,28 +54,55 @@ impl Drop for MTPDevice {
     }
 }
 
-impl MTPDevice {
-    /// Gets the friendly name of this device, e.g. "Kevin's super smartphone"
-    pub fn get_friendly_name(&self) -> Result<String, ErrorKind> {
+impl Debug for MtpDevice {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("MTPDevice")
+            .field("default_music_folder", &self.default_music_folder())
+            .finish()
+    }
+}
+
+impl MtpDevice {
+    pub(crate) fn latest_error(&self) -> Option<Error> {
+        unsafe {
+            let list = ffi::LIBMTP_Get_Errorstack(self.inner);
+            let err = Error::from_latest_error(list)?;
+            ffi::LIBMTP_Clear_Errorstack(self.inner);
+            Some(err)
+        }
+    }
+}
+
+impl MtpDevice {
+    /// Retrieves the default music folder, if there isn't one this value may be garbage.
+    /// Therefore it's not recommended to depend on this value.
+    pub fn default_music_folder(&self) -> u32 {
+        unsafe { (*self.inner).default_music_folder }
+    }
+
+    /// Gets the friendly name of this device, e.g. "Kevin's Android"
+    pub fn get_friendly_name(&self) -> Result<String, Error> {
         unsafe {
             let friendly_name = ffi::LIBMTP_Get_Friendlyname(self.inner);
+
             if friendly_name.is_null() {
-                Err(ErrorKind::Unknown)
+                Err(self.latest_error().unwrap_or_default())
             } else {
-                let vec = c_charp_to_u8v!(friendly_name);
+                let u8vec = cstr_to_u8vec!(friendly_name);
                 libc::free(friendly_name as *mut _);
-                Ok(String::from_utf8(vec)?)
+                Ok(String::from_utf8(u8vec)?)
             }
         }
     }
 
     /// Sets the friendly name of this device
-    pub fn set_friendly_name(&self, name: &str) -> Result<(), ErrorKind> {
+    pub fn set_friendly_name(&self, name: &str) -> Result<(), Error> {
         unsafe {
             let res =
                 ffi::LIBMTP_Set_Friendlyname(self.inner, name.as_ptr() as *const libc::c_char);
-            if let Some(err) = ErrorKind::from_code(res as u32) {
-                Err(err)
+
+            if res != 0 {
+                Err(self.latest_error().unwrap_or_default())
             } else {
                 Ok(())
             }
@@ -87,23 +110,23 @@ impl MTPDevice {
     }
 
     /// Retrieves the synchronization partner of this device.
-    pub fn get_sync_partner(&self) -> Result<String, ErrorKind> {
+    pub fn get_sync_partner(&self) -> Result<String, Error> {
         unsafe {
             let partner = ffi::LIBMTP_Get_Syncpartner(self.inner);
-            let vec = c_charp_to_u8v!(partner);
+            let u8vec = cstr_to_u8vec!(partner);
             libc::free(partner as *mut _);
-            Ok(String::from_utf8(vec)?)
+            Ok(String::from_utf8(u8vec)?)
         }
     }
 
     /// Sets the synchronization partner of this device.
-    pub fn set_sync_partner(&self, partner: &str) -> Result<(), ErrorKind> {
+    pub fn set_sync_partner(&self, partner: &str) -> Result<(), Error> {
         unsafe {
             let res =
                 ffi::LIBMTP_Set_Syncpartner(self.inner, partner.as_ptr() as *const libc::c_char);
 
-            if let Some(err) = ErrorKind::from_code(res as u32) {
-                Err(err)
+            if res != 0 {
+                Err(self.latest_error().unwrap_or_default())
             } else {
                 Ok(())
             }
@@ -111,76 +134,76 @@ impl MTPDevice {
     }
 
     /// Returns the manufacturer name of this device, may fail.
-    pub fn manufacturer_name(&self) -> Result<String, ErrorKind> {
+    pub fn manufacturer_name(&self) -> Result<String, Error> {
         unsafe {
             let manufacturer = ffi::LIBMTP_Get_Manufacturername(self.inner);
+
             if manufacturer.is_null() {
-                Err(ErrorKind::Unknown)
+                Err(self.latest_error().unwrap_or_default())
             } else {
-                let vec = c_charp_to_u8v!(manufacturer);
+                let u8vec = cstr_to_u8vec!(manufacturer);
                 libc::free(manufacturer as *mut _);
-                Ok(String::from_utf8(vec)?)
+                Ok(String::from_utf8(u8vec)?)
             }
         }
     }
 
     /// Returns the model name of this device, may fail.
-    pub fn model_name(&self) -> Result<String, ErrorKind> {
+    pub fn model_name(&self) -> Result<String, Error> {
         unsafe {
             let model = ffi::LIBMTP_Get_Modelname(self.inner);
+
             if model.is_null() {
-                Err(ErrorKind::Unknown)
+                Err(self.latest_error().unwrap_or_default())
             } else {
-                let vec = c_charp_to_u8v!(model);
+                let u8vec = cstr_to_u8vec!(model);
                 libc::free(model as *mut _);
-                Ok(String::from_utf8(vec)?)
+                Ok(String::from_utf8(u8vec)?)
             }
         }
     }
 
     /// Returns the serial number of this device, may fail.
-    pub fn serial_number(&self) -> Result<String, ErrorKind> {
+    pub fn serial_number(&self) -> Result<String, Error> {
         unsafe {
             let serial = ffi::LIBMTP_Get_Serialnumber(self.inner);
+
             if serial.is_null() {
-                Err(ErrorKind::Unknown)
+                Err(self.latest_error().unwrap_or_default())
             } else {
-                let vec = c_charp_to_u8v!(serial);
+                let u8vec = cstr_to_u8vec!(serial);
                 libc::free(serial as *mut _);
-                Ok(String::from_utf8(vec)?)
+                Ok(String::from_utf8(u8vec)?)
             }
         }
     }
 
     /// Returns the device (public key) certificate as an XML document string, may fail.
-    pub fn device_certificate(&self) -> Result<String, ErrorKind> {
+    pub fn device_certificate(&self) -> Result<String, Error> {
         unsafe {
             let mut devcert = std::ptr::null_mut();
             let res = ffi::LIBMTP_Get_Device_Certificate(self.inner, &mut devcert);
 
-            if let Some(err) = ErrorKind::from_code(res as u32) {
-                return Err(err);
+            if res != 0 || devcert.is_null() {
+                Err(self.latest_error().unwrap_or_default())
+            } else {
+                let u8vec = cstr_to_u8vec!(devcert);
+                libc::free(devcert as *mut _);
+                Ok(String::from_utf8(u8vec)?)
             }
-
-            if devcert.is_null() {
-                return Err(ErrorKind::Unknown);
-            }
-
-            let vec = c_charp_to_u8v!(devcert);
-            libc::free(devcert as *mut _);
-            Ok(String::from_utf8(vec)?)
         }
     }
 
     /// Retrieves the current and maximum battery level of this device, may fail.
-    pub fn battery_level(&self) -> Result<(BatteryLevel, u8), ErrorKind> {
+    pub fn battery_level(&self) -> Result<(BatteryLevel, u8), Error> {
         unsafe {
             let mut max_level = 0;
             let mut cur_level = 0;
 
             let res = ffi::LIBMTP_Get_Batterylevel(self.inner, &mut max_level, &mut cur_level);
-            if let Some(err) = ErrorKind::from_code(res as u32) {
-                Err(err)
+
+            if res != 0 {
+                Err(self.latest_error().unwrap_or_default())
             } else {
                 let cur_level = if cur_level == 0 {
                     BatteryLevel::OnExternalPower
@@ -194,49 +217,42 @@ impl MTPDevice {
     }
 
     /// Returns the secure time as an XML document string, may fail.
-    pub fn secure_time(&self) -> Result<String, ErrorKind> {
+    pub fn secure_time(&self) -> Result<String, Error> {
         unsafe {
             let mut secure_time = std::ptr::null_mut();
             let res = ffi::LIBMTP_Get_Secure_Time(self.inner, &mut secure_time);
 
-            if let Some(err) = ErrorKind::from_code(res as u32) {
-                return Err(err);
+            if res != 0 || secure_time.is_null() {
+                Err(self.latest_error().unwrap_or_default())
+            } else {
+                let u8vec = cstr_to_u8vec!(secure_time);
+                libc::free(secure_time as *mut _);
+                Ok(String::from_utf8(u8vec)?)
             }
-
-            if secure_time.is_null() {
-                return Err(ErrorKind::Unknown);
-            }
-
-            let vec = c_charp_to_u8v!(secure_time);
-            libc::free(secure_time as *mut _);
-            Ok(String::from_utf8(vec)?)
         }
     }
 
     /// Retrieves a list of supported file types that this device claims it supports.  
     /// This list is mitigated to include the file types that `libmtp` (C library) can handle.
-    pub fn supported_filetypes(&self) -> Result<Vec<Filetype>, ErrorKind> {
+    pub fn supported_filetypes(&self) -> Result<Vec<Filetype>, Error> {
         unsafe {
             let mut filetypes = std::ptr::null_mut();
             let mut len = 0;
 
             let res = ffi::LIBMTP_Get_Supported_Filetypes(self.inner, &mut filetypes, &mut len);
-            if let Some(err) = ErrorKind::from_code(res as u32) {
-                return Err(err);
-            }
 
-            if filetypes.is_null() {
-                return Err(ErrorKind::Unknown);
-            }
+            if res != 0 || filetypes.is_null() {
+                Err(self.latest_error().unwrap_or_default())
+            } else {
+                let mut filetypes_vec = Vec::with_capacity(len as usize);
+                for i in 0..(len as isize) {
+                    let ftype = Filetype::from_u16(*filetypes.offset(i)).unwrap();
+                    filetypes_vec.push(ftype);
+                }
 
-            let mut filetypes_vec = Vec::with_capacity(len as usize);
-            for i in 0..(len as isize) {
-                let ftype = Filetype::from_u16(*filetypes.offset(i)).unwrap();
-                filetypes_vec.push(ftype);
+                libc::free(filetypes as *mut _);
+                Ok(filetypes_vec)
             }
-
-            libc::free(filetypes as *mut _);
-            Ok(filetypes_vec)
         }
     }
 
@@ -251,11 +267,12 @@ impl MTPDevice {
 
     /// Reset the device only is this one supports the `PTP_OC_ResetDevice` operation code
     /// (`0x1010`)
-    pub fn reset_device(&self) -> Result<(), ErrorKind> {
+    pub fn reset_device(&self) -> Result<(), Error> {
         unsafe {
             let res = ffi::LIBMTP_Reset_Device(self.inner);
-            if let Some(err) = ErrorKind::from_code(res as u32) {
-                Err(err)
+
+            if res != 0 {
+                Err(self.latest_error().unwrap_or_default())
             } else {
                 Ok(())
             }
@@ -265,26 +282,26 @@ impl MTPDevice {
     /// Updates all the internal storage ids and properties of this device, it can also
     /// optionally sort the list. This operation may success, partially success
     /// (only ids were retrieved) or fail.
-    pub fn update_storage(&self, sort_by: StorageSort) -> UpdateStorage {
+    pub fn update_storage(&mut self, sort_by: StorageSort) -> Result<UpdateStorage, Error> {
         unsafe {
             let res = ffi::LIBMTP_Get_Storage(self.inner, sort_by.to_i32().unwrap());
             match res {
-                0 => UpdateStorage::Success,
-                1 => UpdateStorage::OnlyIds,
-                _ => UpdateStorage::Failure,
+                0 => Ok(UpdateStorage::Success),
+                1 => Ok(UpdateStorage::OnlyIds),
+                _ => Err(self.latest_error().unwrap_or_default()),
             }
         }
     }
 
-    /// Returns the inner storage entries, you need to call this if you updated
-    /// the storage with `update_storage`.
-    pub fn storage_entries(&self) -> Vec<Storage> {
+    /// Returns the inner storage pool, you need to call this if you updated
+    /// the storage with `update_storage`. Note that the pool may be empty.
+    pub fn storage_pool(&self) -> StoragePool<'_> {
         unsafe {
             let mut storage = (*self.inner).storage;
 
-            let mut entries = Vec::new();
+            let mut pool = Vec::new();
             while !storage.is_null() {
-                entries.push(Storage {
+                pool.push(Storage {
                     inner: storage,
                     owner: &self,
                 });
@@ -292,7 +309,7 @@ impl MTPDevice {
                 storage = (*storage).next;
             }
 
-            entries
+            StoragePool { pool, owner: &self }
         }
     }
 
@@ -304,34 +321,12 @@ impl MTPDevice {
         }
     }
 
-    /// Returns a list of the stack errors.
-    pub fn error_stack(&self) -> Vec<StackError> {
-        unsafe {
-            let list = ffi::LIBMTP_Get_Errorstack(self.inner);
-            StackError::from_error_list(list)
-        }
-    }
-
-    /// Dumps the error stack to `stderr`, beware that this doesn't clean the error stack.
-    pub fn dump_error_stack(&self) {
-        unsafe {
-            ffi::LIBMTP_Dump_Errorstack(self.inner);
-        }
-    }
-
-    /// Clears the error stack.
-    pub fn clear_error_stack(&self) {
-        unsafe {
-            ffi::LIBMTP_Clear_Errorstack(self.inner);
-        }
-    }
-
     /// Determines wheter a property is supported for a given file type.
     pub fn is_property_supported(
         &self,
         property: Property,
         filetype: Filetype,
-    ) -> Result<bool, ErrorKind> {
+    ) -> Result<bool, Error> {
         let property = property.to_u32().unwrap();
         let filetype = filetype.to_u32().unwrap();
 
@@ -340,16 +335,53 @@ impl MTPDevice {
             match res {
                 0 => Ok(false),
                 r if r > 0 => Ok(true),
-                _ => Err(ErrorKind::Unknown),
+                _ => Err(self.latest_error().unwrap_or_default()),
             }
         }
     }
 
-    pub fn allowed_property_values(&self, property: Property, filetype: Filetype) {
+    /// Retrieves the allowes values (range or enumeration) for an specific property.
+    pub fn allowed_property_values(
+        &self,
+        property: Property,
+        filetype: Filetype,
+    ) -> Result<AllowedValues, Error> {
         let property = property.to_u32().unwrap();
         let filetype = filetype.to_u32().unwrap();
+
+        unsafe {
+            let allowed_values_ptr = std::ptr::null_mut();
+
+            let res = ffi::LIBMTP_Get_Allowed_Property_Values(
+                self.inner,
+                property,
+                filetype,
+                allowed_values_ptr,
+            );
+
+            if res != 0 || allowed_values_ptr.is_null() {
+                Err(self.latest_error().unwrap_or_default())
+            } else {
+                let allowed_values =
+                    AllowedValues::from_raw(allowed_values_ptr).ok_or_else(|| Error::Unknown)?;
+                ffi::LIBMTP_destroy_allowed_values_t(allowed_values_ptr);
+                Ok(allowed_values)
+            }
+        }
     }
 
     // TODO: Custom operation function (c_variadic)
     // pub fn custom_operation(&self, code: u16, params: &[u32]) -> Result<(), ErrorKind>;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::basic::raw::detect_raw_devices;
+
+    #[test]
+    fn temp() {
+        let raw_devices = detect_raw_devices().unwrap();
+        let mtp_device = raw_devices[0].open_uncached().unwrap();
+        println!("{:?}", mtp_device);
+    }
 }
