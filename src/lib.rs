9afac0d7ca01a86a2a6a986a15668e32a4c67472
Kevin Del Castillo <quebin31@gmail.com>
2020-09-28 15:52:47 -0500

    Declare modules.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
+use error::Error;
+
 #[macro_use]
 mod macros;
 
-pub mod basic;
-pub mod capabilities;
 pub mod error;
-pub mod filetypes;
 pub mod internals;
-pub mod properties;
+pub mod util;
+
+pub mod values;
+
+pub mod device;
+pub mod object;
+
+pub type Result<T> = std::result::Result<T, Error>;
