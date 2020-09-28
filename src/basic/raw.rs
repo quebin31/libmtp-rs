3c647081a7afd46c8a6b5c93514994cb24db40eb
Kevin Del Castillo <quebin31@gmail.com>
2020-09-27 23:32:04 -0500

    Refactor in order to use the new Error.

--- src/basic/raw.rs
+++ src/basic/raw.rs
@@ -1,7 +1,7 @@
 use std::mem::MaybeUninit;
 
-use super::device::MTPDevice;
-use crate::{error::ErrorKind, internals::maybe_init};
+use super::device::MtpDevice;
+use crate::{error::Error, error::MtpErrorKind, internals::maybe_init};
 use libmtp_sys as ffi;
 
 /// Raw MTP device descriptor, used to manually open an MTP device
@@ -12,7 +12,7 @@ pub struct RawDevice {
 impl RawDevice {
     /// Open an MTP device from this raw device descriptor, this method
     /// may cache devices, thus may be slower.
-    pub fn open(&self) -> Option<MTPDevice> {
+    pub fn open(&self) -> Option<MtpDevice> {
         unsafe {
             let ptr = &self.inner as *const _;
             let device = ffi::LIBMTP_Open_Raw_Device(ptr as *mut _);
@@ -20,13 +20,13 @@ impl RawDevice {
             if device.is_null() {
                 None
             } else {
-                Some(MTPDevice { inner: device })
+                Some(MtpDevice { inner: device })
             }
         }
     }
 
     /// Open an MTP device from this raw device descriptor, uncached version.
-    pub fn open_uncached(&self) -> Option<MTPDevice> {
+    pub fn open_uncached(&self) -> Option<MtpDevice> {
         unsafe {
             let ptr = &self.inner as *const _;
             let device = ffi::LIBMTP_Open_Raw_Device_Uncached(ptr as *mut _);
@@ -34,14 +34,14 @@ impl RawDevice {
             if device.is_null() {
                 None
             } else {
-                Some(MTPDevice { inner: device })
+                Some(MtpDevice { inner: device })
             }
         }
     }
 }
 
 /// Detect the raw MTP device descriptors and return a vector of the devices found.
-pub fn detect_raw_devices() -> Result<Vec<RawDevice>, ErrorKind> {
+pub fn detect_raw_devices() -> Result<Vec<RawDevice>, Error> {
     maybe_init();
 
     unsafe {
@@ -49,19 +49,23 @@ pub fn detect_raw_devices() -> Result<Vec<RawDevice>, ErrorKind> {
         let mut len = 0;
 
         let res = ffi::LIBMTP_Detect_Raw_Devices(&mut devices, &mut len);
-        if let Some(err) = ErrorKind::from_code(res) {
-            return Err(err);
+
+        if let Some(kind) = MtpErrorKind::from_error_number(res) {
+            Err(Error::MtpError {
+                kind,
+                text: "Failed to detect raw devices".to_string(),
+            })
+        } else {
+            let mut devices_vec = Vec::with_capacity(len as usize);
+            for i in 0..(len as isize) {
+                let mut new = MaybeUninit::zeroed().assume_init();
+
+                std::ptr::copy_nonoverlapping(devices.offset(i), &mut new, 1);
+                devices_vec.push(RawDevice { inner: new });
+            }
+
+            libc::free(devices as *mut _);
+            Ok(devices_vec)
         }
-
-        let mut devices_vec = Vec::with_capacity(len as usize);
-        for i in 0..(len as isize) {
-            let mut new = MaybeUninit::zeroed().assume_init();
-
-            std::ptr::copy_nonoverlapping(devices.offset(i), &mut new, 1);
-            devices_vec.push(RawDevice { inner: new });
-        }
-
-        libc::free(devices as *mut _);
-        Ok(devices_vec)
     }
 }
