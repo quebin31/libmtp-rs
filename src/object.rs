c56abe2461a660495b59b6c3af6c8eb850191f36
Kevin Del Castillo <quebin31@gmail.com>
2020-09-29 22:25:33 -0500

    New trait Object replaces old struct Object. The idea is to implement this trait in almost every "object" from libmtp (like files, tracks, etc).  Furthermore, this allows to more indirection on ids with method id(). Also added a trait AsObjectId, allows to use Objects and u32 as ids.

--- src/object.rs
+++ src/object.rs
@@ -6,37 +6,46 @@ pub mod properties;
 
 use super::device::MtpDevice;
 
-use crate::{error::Error, Identifiable};
+use crate::Result;
 
 use libmtp_sys as ffi;
 use num_traits::ToPrimitive;
 use properties::Property;
 
-/// Object handler, allows to get and set values on properties, this handler
-/// is tied with an MTP device.
-#[derive(Debug, Copy, Clone)]
-pub struct Object<'a> {
-    pub(crate) id: u32,
-    pub(crate) owner: &'a MtpDevice,
+pub trait AsObjectId {
+    fn as_id(&self) -> u32;
 }
 
-impl Identifiable for Object<'_> {
-    type Id = u32;
-
-    fn id(&self) -> Self::Id {
-        self.id
+impl<T> AsObjectId for T
+where
+    T: Object,
+{
+    fn as_id(&self) -> u32 {
+        self.id()
     }
 }
 
-impl<'a> Object<'a> {
-    /// Retrieves a string from an object attribute, may fail.
-    pub fn get_string(&self, property: Property) -> Result<String, Error> {
-        let property = property.to_u32().expect("Unexpected Property variant");
+impl AsObjectId for u32 {
+    fn as_id(&self) -> u32 {
+        *self
+    }
+}
+
+pub trait Object {
+    fn id(&self) -> u32;
+    fn device(&self) -> &MtpDevice;
+
+    /// Retrieves a string from an object attribute.
+    fn get_string(&self, property: Property) -> Result<String> {
+        let property = property.to_u32().unwrap();
+        let id = self.id();
+        let device = self.device();
+
         unsafe {
-            let string = ffi::LIBMTP_Get_String_From_Object(self.owner.inner, self.id, property);
+            let string = ffi::LIBMTP_Get_String_From_Object(device.inner, id, property);
 
             if string.is_null() {
-                Err(self.owner.latest_error().unwrap_or_default())
+                Err(device.latest_error().unwrap_or_default())
             } else {
                 let u8vec = cstr_to_u8vec!(string);
                 libc::free(string as *mut _);
@@ -45,32 +54,38 @@ impl<'a> Object<'a> {
         }
     }
 
-    /// Sets an object attribute from a string, may fail.
-    pub fn set_string(&self, property: Property, string: &str) -> Result<(), Error> {
-        let property = property.to_u32().expect("Unexpected Property variant");
+    /// Sets an object attribute from a string.
+    fn set_string(&self, property: Property, string: &str) -> Result<()> {
+        let property = property.to_u32().unwrap();
+        let id = self.id();
+        let device = self.device();
+
         unsafe {
             let res = ffi::LIBMTP_Set_Object_String(
-                self.owner.inner,
-                self.id,
+                device.inner,
+                id,
                 property,
                 string.as_ptr() as *const _,
             );
 
             if res != 0 {
-                Err(self.owner.latest_error().unwrap_or_default())
+                Err(device.latest_error().unwrap_or_default())
             } else {
                 Ok(())
             }
         }
     }
 
-    /// Retrieves an `u64` from an object attribute, may fail.
-    pub fn get_u64(&self, property: Property) -> Result<u64, Error> {
-        let property = property.to_u32().expect("Unexpected Property variant");
-        unsafe {
-            let val = ffi::LIBMTP_Get_u64_From_Object(self.owner.inner, self.id, property, 0);
+    /// Retrieves an `u64` from an object attribute.
+    fn get_u64(&self, property: Property) -> Result<u64> {
+        let property = property.to_u32().unwrap();
+        let id = self.id();
+        let device = self.device();
 
-            if let Some(err) = self.owner.latest_error() {
+        unsafe {
+            let val = ffi::LIBMTP_Get_u64_From_Object(device.inner, id, property, 0);
+
+            if let Some(err) = device.latest_error() {
                 Err(err)
             } else {
                 Ok(val)
@@ -79,12 +94,15 @@ impl<'a> Object<'a> {
     }
 
     /// Retrieves an `u32` from an object attribute, returns the value of `default` on failure.
-    pub fn get_u32(&self, property: Property) -> Result<u32, Error> {
-        let property = property.to_u32().expect("Unexpected Property variant");
-        unsafe {
-            let val = ffi::LIBMTP_Get_u32_From_Object(self.owner.inner, self.id, property, 0);
+    fn get_u32(&self, property: Property) -> Result<u32> {
+        let property = property.to_u32().unwrap();
+        let id = self.id();
+        let device = self.device();
 
-            if let Some(err) = self.owner.latest_error() {
+        unsafe {
+            let val = ffi::LIBMTP_Get_u32_From_Object(device.inner, id, property, 0);
+
+            if let Some(err) = device.latest_error() {
                 Err(err)
             } else {
                 Ok(val)
@@ -92,14 +110,17 @@ impl<'a> Object<'a> {
         }
     }
 
-    /// Sets an object attribute from an `u32`, may fail.
-    pub fn set_u32(&self, property: Property, value: u32) -> Result<(), Error> {
-        let property = property.to_u32().expect("Unexpected Property variant");
+    /// Sets an object attribute from an `u32`.
+    fn set_u32(&self, property: Property, value: u32) -> Result<()> {
+        let property = property.to_u32().unwrap();
+        let id = self.id();
+        let device = self.device();
+
         unsafe {
-            let res = ffi::LIBMTP_Set_Object_u32(self.owner.inner, self.id, property, value);
+            let res = ffi::LIBMTP_Set_Object_u32(device.inner, id, property, value);
 
             if res != 0 {
-                Err(self.owner.latest_error().unwrap_or_default())
+                Err(device.latest_error().unwrap_or_default())
             } else {
                 Ok(())
             }
@@ -107,12 +128,15 @@ impl<'a> Object<'a> {
     }
 
     /// Retrieves an `u16` from an object attribute, returns the value of `default` on failure.
-    pub fn get_u16(&self, property: Property) -> Result<u16, Error> {
-        let property = property.to_u32().expect("Unexpected Property variant");
-        unsafe {
-            let val = ffi::LIBMTP_Get_u16_From_Object(self.owner.inner, self.id, property, 0);
+    fn get_u16(&self, property: Property) -> Result<u16> {
+        let property = property.to_u32().unwrap();
+        let id = self.id();
+        let device = self.device();
 
-            if let Some(err) = self.owner.latest_error() {
+        unsafe {
+            let val = ffi::LIBMTP_Get_u16_From_Object(device.inner, id, property, 0);
+
+            if let Some(err) = device.latest_error() {
                 Err(err)
             } else {
                 Ok(val)
@@ -120,14 +144,17 @@ impl<'a> Object<'a> {
         }
     }
 
-    /// Sets an object attribute from an `u16`, may fail.
-    pub fn set_u16(&self, property: Property, value: u16) -> Result<(), Error> {
-        let property = property.to_u32().expect("Unexpected Property variant");
+    /// Sets an object attribute from an `u16`.
+    fn set_u16(&self, property: Property, value: u16) -> Result<()> {
+        let property = property.to_u32().unwrap();
+        let id = self.id();
+        let device = self.device();
+
         unsafe {
-            let res = ffi::LIBMTP_Set_Object_u16(self.owner.inner, self.id, property, value);
+            let res = ffi::LIBMTP_Set_Object_u16(device.inner, id, property, value);
 
             if res != 0 {
-                Err(self.owner.latest_error().unwrap_or_default())
+                Err(device.latest_error().unwrap_or_default())
             } else {
                 Ok(())
             }
@@ -135,12 +162,15 @@ impl<'a> Object<'a> {
     }
 
     /// Retrieves an `u8` from an object attribute, returns the value of `default` on failure.
-    pub fn get_u8(&self, property: Property) -> Result<u8, Error> {
-        let property = property.to_u32().expect("Unexpected Property variant");
-        unsafe {
-            let val = ffi::LIBMTP_Get_u8_From_Object(self.owner.inner, self.id, property, 0);
+    fn get_u8(&self, property: Property) -> Result<u8> {
+        let property = property.to_u32().unwrap();
+        let id = self.id();
+        let device = self.device();
 
-            if let Some(err) = self.owner.latest_error() {
+        unsafe {
+            let val = ffi::LIBMTP_Get_u8_From_Object(device.inner, id, property, 0);
+
+            if let Some(err) = device.latest_error() {
                 Err(err)
             } else {
                 Ok(val)
@@ -148,14 +178,17 @@ impl<'a> Object<'a> {
         }
     }
 
-    /// Sets an object attribute from an `u8`, may fail.
-    pub fn set_u8(&self, property: Property, value: u8) -> Result<(), Error> {
-        let property = property.to_u32().expect("Unexpected Property variant");
+    /// Sets an object attribute from an `u8`.
+    fn set_u8(&self, property: Property, value: u8) -> Result<()> {
+        let property = property.to_u32().unwrap();
+        let id = self.id();
+        let device = self.device();
+
         unsafe {
-            let res = ffi::LIBMTP_Set_Object_u8(self.owner.inner, self.id, property, value);
+            let res = ffi::LIBMTP_Set_Object_u8(device.inner, id, property, value);
 
             if res != 0 {
-                Err(self.owner.latest_error().unwrap_or_default())
+                Err(device.latest_error().unwrap_or_default())
             } else {
                 Ok(())
             }
