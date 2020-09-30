e6e703b01e8b16aaedc2ce53cc488de6d7c80512
Kevin Del Castillo <quebin31@gmail.com>
2020-09-29 22:27:46 -0500

    Remove Identifiable trait in favor of Object and AsObjectId traits.

--- src/lib.rs
+++ src/lib.rs
@@ -11,23 +11,10 @@ pub mod values;
 
 pub mod device;
 pub mod object;
+pub mod storage;
 
 /// Re-export for support convenience.
 pub use chrono;
 
 /// Custom Result, used (almost) across all the crate.
 pub type Result<T> = std::result::Result<T, Error>;
-
-pub trait Identifiable {
-    type Id;
-
-    fn id(&self) -> Self::Id;
-}
-
-impl Identifiable for u32 {
-    type Id = u32;
-
-    fn id(&self) -> Self::Id {
-        *self
-    }
-}
