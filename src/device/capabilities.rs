13da10e476af32fb77f34d3a9ca1c4430f3d4523
Kevin Del Castillo <quebin31@gmail.com>
2020-09-30 19:36:17 -0500

    Add documentation on device capabilities.

--- src/device/capabilities.rs
+++ src/device/capabilities.rs
@@ -1,6 +1,10 @@
+//! Describes the device capabilities, some devices may not necessarily implement
+//! or support certain capabilities, like copying or moving objects.
+
 use num_derive::{FromPrimitive, ToPrimitive};
 
-/// Possible device capabilities.
+/// Supported `libmtp` device capabilities, you can test if an MTP device supports
+/// one of those with [`MtpDevice::check_capability`](../struct.MtpDevice.html#method.check_capability)
 #[derive(Debug, Clone, FromPrimitive, ToPrimitive)]
 pub enum DeviceCapability {
     /// This capability tells whether you can get partial objects.
