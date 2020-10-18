59b7e144d043d52e3e3bacf39488d935bad2b100
Kevin Del Castillo <quebin31@gmail.com>
2020-10-17 23:49:52 -0500

    Added examples on how to get and send files.

---
+++ examples/get_file.rs
@@ -0,0 +1,58 @@
+use anyhow::Error;
+use libmtp_rs::{
+    device::{raw::detect_raw_devices, StorageSort},
+    object::filetypes::Filetype,
+    storage::Parent,
+    util::CallbackReturn,
+};
+use std::io::Write;
+use text_io::read;
+
+fn main() -> Result<(), Error> {
+    let raw_devices = detect_raw_devices()?;
+    let mtp_device = if let Some(raw) = raw_devices.get(0) {
+        raw.open_uncached()
+    } else {
+        println!("No devices");
+        return Ok(());
+    };
+
+    if let Some(mut mtp_device) = mtp_device {
+        mtp_device.update_storage(StorageSort::ByFreeSpace)?;
+
+        let storage_pool = mtp_device.storage_pool();
+        let (_, storage) = storage_pool.iter().next().expect("No storage");
+
+        let root_contents: Vec<_> = storage
+            .files_and_folders(Parent::Root)
+            .into_iter()
+            .filter(|file| !matches!(file.ftype(), Filetype::Folder))
+            .collect();
+
+        let no_digits = root_contents.len().to_string().len();
+
+        for (idx, file) in root_contents.iter().enumerate() {
+            println!("{:>d$}) {}", idx, file.name(), d = no_digits);
+        }
+
+        print!("Choose a file (type a number): ");
+        std::io::stdout().lock().flush()?;
+        let choosen: usize = read!();
+
+        if let Some(file) = root_contents.get(choosen) {
+            storage.get_file_to_path(file, file.name(), |sent, total| {
+                print!("\rProgress {}/{}", sent, total);
+                std::io::stdout().lock().flush().expect("Failed to flush");
+                CallbackReturn::Continue
+            })?;
+
+            println!();
+        } else {
+            println!("Invalid selection!");
+        }
+    } else {
+        println!("Couldn't open device");
+    }
+
+    Ok(())
+}
---
+++ examples/send_file.rs
@@ -0,0 +1,54 @@
+use anyhow::Error;
+use libmtp_rs::object::filetypes::Filetype;
+use libmtp_rs::storage::{files::FileMetadata, Parent};
+use libmtp_rs::{
+    device::{raw::detect_raw_devices, StorageSort},
+    util::CallbackReturn,
+};
+use std::{fs::File, io::Write, path::Path};
+
+fn main() -> Result<(), Error> {
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() != 2 {
+        println!("Error: please provide a path to the file to send");
+        return Ok(());
+    }
+
+    let raw_devices = detect_raw_devices()?;
+    let mtp_device = if let Some(raw) = raw_devices.get(0) {
+        raw.open_uncached()
+    } else {
+        println!("No devices");
+        return Ok(());
+    };
+
+    if let Some(mut mtp_device) = mtp_device {
+        mtp_device.update_storage(StorageSort::ByFreeSpace)?;
+
+        let storage_pool = mtp_device.storage_pool();
+        let (_, storage) = storage_pool.iter().next().expect("No storage");
+
+        let path = Path::new(&args[1]);
+        let file = File::open(path)?;
+        let metadata = file.metadata()?;
+
+        let metadata = FileMetadata {
+            file_size: metadata.len(),
+            file_name: path.file_name().unwrap().to_str().expect("Invalid UTF-8"),
+            file_type: Filetype::Text,
+            modification_date: metadata.modified()?.into(),
+        };
+
+        storage.send_file_from_path(path, Parent::Root, metadata, |sent, total| {
+            print!("\rProgress {}/{}", sent, total);
+            std::io::stdout().lock().flush().expect("Failed to flush");
+            CallbackReturn::Continue
+        })?;
+
+        println!()
+    } else {
+        println!("Couldn't open device");
+    }
+
+    Ok(())
+}
